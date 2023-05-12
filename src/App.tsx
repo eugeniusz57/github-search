d3c05cada3c64e77f43583d4a77d0d785be8fa59
eugeniusz57 <zenakaveckij1@gmail.com>
2023-05-12 17:45:44 +0200

    add Header and Switcher theme on App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import { Container } from "./components/Container";
+import { TheHeader } from "./components/TheHeader";
 
 function App() {
   return (
     <Container>
-      <p>Hello</p>
+     <TheHeader/>
     </Container>
   );
 }
