37b97f46ca8474e0d38aa9bc74bee26f7143acb3
eugeniusz57 <zenakaveckij1@gmail.com>
2023-05-14 10:49:24 +0200

    add user: card, info; search

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,41 @@
-import { Container } from "./components/Container";
-import { TheHeader } from "./components/TheHeader";
+import { useState } from 'react';
+
+import { Container } from './components/Container';
+import { Search } from './components/Search';
+import { TheHeader } from './components/TheHeader';
+import { UserCard } from './components/UserCard';
+import { GithubError, GithubUser, LocalGithubUser } from './types';
+import { extractLocalUser } from './utils/exract-local-user';
+import { isGithubUser } from './utils/typeguards';
+
+const BASE_URL = 'https://api.github.com/users/';
 
 function App() {
+
+  const [user, setUser] = useState<LocalGithubUser | null>();
+
+  const fetchUser = async (username: string) => {
+    const url = BASE_URL + username;
+
+    const res = await fetch(url);
+    const user = await res.json() as GithubUser | GithubError;
+
+    if (isGithubUser(user)) {
+      setUser(extractLocalUser(user));
+    } else {
+      setUser(null);
+    }
+  }
+
   return (
-    <Container>
-     <TheHeader/>
+     <Container>
+      <TheHeader />
+      <Search hasError={!user} onSubmit={fetchUser}/>
+      {user && (
+        <UserCard
+          {...user}
+        />
+      )}
     </Container>
   );
 }
